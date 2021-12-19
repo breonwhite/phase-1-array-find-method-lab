b35d108a432d01d12bedaf7fa88ce95d45e8a8d0
Breon White <bdwhite92@gmail.com>
2021-12-19 14:38:07 -0500

    commit tree c9eaf463ac309611e7cf5f6613d17c22d4e421a1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function findWin(object){
+    return object.result === "W"
+}
+function superbowlWin(array){
+    const foundItem = array.find(findWin);
+    if (foundItem){
+        return foundItem.year;
+    }
+}
+console.log(superbowlWin(record));
